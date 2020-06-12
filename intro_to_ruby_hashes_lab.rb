76430c743547ea6be39fd322fff48339c5f34ef5
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-12 03:32:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -78,9 +78,13 @@ def monopoly_with_fourth_tier
           },
           :shortline_railroad => {
             :mortgage_value => 800
+            
           }
+          
         }
+        
       }
+    
   }
  base
 end
